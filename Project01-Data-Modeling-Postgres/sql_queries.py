5040000e346be482c973101938ab4ec04747ad0a
Kroudir <a_kroudir@esi.dz>
2020-04-02 20:07:56 +0100

    Project 01: Fixed update the user level in new entry

--- Project01-Data-Modeling-Postgres/sql_queries.py
+++ Project01-Data-Modeling-Postgres/sql_queries.py
@@ -81,9 +81,7 @@ user_table_insert = ("""
     INSERT INTO users
     (user_id, first_name, last_name, gender, level)
     VALUES (%s, %s, %s, %s, %s)
-    ON CONFLICT (user_id) 
-    DO UPDATE
-    SET level = users.level;
+    ON CONFLICT (user_id) DO UPDATE SET level = EXCLUDED.level;
 """)
 
 song_table_insert = ("""
