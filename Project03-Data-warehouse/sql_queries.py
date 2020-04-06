bcfaf3219d21d424ae10785920a8abb2ff55b2ae
Amir KROUDIR <a_kroudir@esi.dz>
2020-04-06 19:40:44 +0100

    sqlqueries: Done the create and drop tables sqlqueries

--- Project03-Data-warehouse/sql_queries.py
+++ Project03-Data-warehouse/sql_queries.py
@@ -7,35 +7,108 @@ config.read('dwh.cfg')
 
 # DROP TABLES
 
-staging_events_table_drop = ""
-staging_songs_table_drop = ""
-songplay_table_drop = ""
-user_table_drop = ""
-song_table_drop = ""
-artist_table_drop = ""
-time_table_drop = ""
+staging_events_table_drop = "DROP TABLE IF EXISTS staging_events;"
+staging_songs_table_drop = "DROP TABLE IF EXISTS staging_songs;"
+songplay_table_drop = "DROP TABLE IF EXISTS songplays;"
+user_table_drop = "DROP TABLE IF EXISTS users;"
+song_table_drop = "DROP TABLE IF EXISTS songs;"
+artist_table_drop = "DROP TABLE IF EXISTS artists;"
+time_table_drop = "DROP TABLE IF EXISTS time;"
 
-# CREATE TABLES
+# CREATE TABLE
 
 staging_events_table_create= ("""
+    CREATE TABLE staging_events(
+        artist              VARCHAR,
+        auth                VARCHAR,
+        firstName           VARCHAR,
+        gender              VARCHAR,
+        itemInSession       INTEGER,
+        lastName            VARCHAR,
+        length              FLOAT,
+        level               VARCHAR,
+        location            VARCHAR,
+        method              VARCHAR,
+        page                VARCHAR,
+        registration        FLOAT,
+        sessionId           INTEGER,
+        song                VARCHAR,
+        status              INTEGER,
+        ts                  TIMESTAMP,
+        userAgent           VARCHAR,
+        userId              INTEGER 
+    )
 """)
 
 staging_songs_table_create = ("""
+    CREATE TABLE staging_songs(
+        num_songs           INTEGER,
+        artist_id           VARCHAR,
+        artist_latitude     FLOAT,
+        artist_longitude    FLOAT,
+        artist_location     VARCHAR,
+        artist_name         VARCHAR,
+        song_id             VARCHAR,
+        title               VARCHAR,
+        duration            FLOAT,
+        year                INTEGER
+    )
 """)
 
 songplay_table_create = ("""
+    CREATE TABLE songplays(
+        songplay_id         INTEGER         IDENTITY(0,1)   PRIMARY KEY,
+        start_time          TIMESTAMP       NOT NULL SORTKEY DISTKEY,
+        user_id             INTEGER         NOT NULL,
+        level               VARCHAR,
+        song_id             VARCHAR         NOT NULL,
+        artist_id           VARCHAR         NOT NULL,
+        session_id          INTEGER,
+        location            VARCHAR,
+        user_agent          VARCHAR
+    )
 """)
 
 user_table_create = ("""
+    CREATE TABLE users(
+        user_id             INTEGER         NOT NULL SORTKEY PRIMARY KEY,
+        first_name          VARCHAR         NOT NULL,
+        last_name           VARCHAR         NOT NULL,
+        gender              VARCHAR         NOT NULL,
+        level               VARCHAR         NOT NULL
+    )
 """)
 
 song_table_create = ("""
+    CREATE TABLE songs(
+        song_id             VARCHAR         NOT NULL SORTKEY PRIMARY KEY,
+        title               VARCHAR         NOT NULL,
+        artist_id           VARCHAR         NOT NULL,
+        year                INTEGER         NOT NULL,
+        duration            FLOAT
+    )
 """)
 
 artist_table_create = ("""
+    CREATE TABLE artists(
+        artist_id           VARCHAR         NOT NULL SORTKEY PRIMARY KEY,
+        name                VARCHAR         NOT NULL,
+        location            VARCHAR,
+        latitude            FLOAT,
+        longitude           FLOAT
+    )
 """)
 
 time_table_create = ("""
+    CREATE TABLE time(
+        start_time          TIMESTAMP       NOT NULL DISTKEY SORTKEY PRIMARY KEY,
+        hour                INTEGER         NOT NULL,
+        day                 INTEGER         NOT NULL,
+        week                INTEGER         NOT NULL,
+        month               INTEGER         NOT NULL,
+        year                INTEGER         NOT NULL,
+        weekday             VARCHAR(20)     NOT NULL
+    )
 """)
 
 # STAGING TABLES
