16bfb123a66b33197e02e59afeb4bdf8f8732186
Amir KROUDIR <a_kroudir@esi.dz>
2020-04-06 23:05:48 +0100

    gitignore : Updated gitignore to ignore all python related non necessary files
    createTable: Updated create table with logs and tested it.

--- Project03-Data-warehouse/create_tables.py
+++ Project03-Data-warehouse/create_tables.py
@@ -20,13 +20,17 @@ def main():
     config.read('dwh.cfg')
 
     conn = psycopg2.connect("host={} dbname={} user={} password={} port={}".format(*config['CLUSTER'].values()))
+    print('1. Connected \n')
     cur = conn.cursor()
+    print('2. Created Cursor \n')
 
     drop_tables(cur, conn)
+    print('3. Dropped tables if already exists \n')
     create_tables(cur, conn)
+    print('4.Created tables \n')
 
     conn.close()
-
+    print('5. Closed the connection \n')
 
 if __name__ == "__main__":
     main()
