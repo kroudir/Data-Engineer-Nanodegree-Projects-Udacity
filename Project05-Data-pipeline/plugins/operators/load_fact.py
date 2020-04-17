173eeab3c3947e7393acd3b8685ee12e1bf6c148
Kroudir <a_kroudir@esi.dz>
2020-04-17 09:40:03 +0100

    feat:Implement LoadFactOperator

--- Project05-Data-pipeline/plugins/operators/load_fact.py
+++ Project05-Data-pipeline/plugins/operators/load_fact.py
@@ -2,21 +2,23 @@ from airflow.hooks.postgres_hook import PostgresHook
 from airflow.models import BaseOperator
 from airflow.utils.decorators import apply_defaults
 
+
 class LoadFactOperator(BaseOperator):
 
     ui_color = '#F98866'
 
     @apply_defaults
     def __init__(self,
-                 # Define your operators params (with defaults) here
-                 # Example:
-                 # conn_id = your-connection-name
+                 aws_credentials_id="",
+                 redshift_conn_id="",
+                 sql_query="",
                  *args, **kwargs):
 
         super(LoadFactOperator, self).__init__(*args, **kwargs)
-        # Map params here
-        # Example:
-        # self.conn_id = conn_id
+        self.aws_credentials_id = aws_credentials_id,
+        self.redshift_conn_id = redshift_conn_id,
+        self.sql_query = sql_query,
 
     def execute(self, context):
-        self.log.info('LoadFactOperator not implemented yet')
+        redshift_hook = PostgresHook(self.redshift_conn_id)
+        redshift_hook.run(str(self.sql_query))
