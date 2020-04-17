fbdfe2feb73b025e43e5261e34527022751116dd
Kroudir <a_kroudir@esi.dz>
2020-04-17 09:44:12 +0100

    feat:Implement LoadDimentionOperator

--- Project05-Data-pipeline/plugins/operators/load_dimension.py
+++ Project05-Data-pipeline/plugins/operators/load_dimension.py
@@ -2,21 +2,32 @@ from airflow.hooks.postgres_hook import PostgresHook
 from airflow.models import BaseOperator
 from airflow.utils.decorators import apply_defaults
 
+
 class LoadDimensionOperator(BaseOperator):
 
     ui_color = '#80BD9E'
 
     @apply_defaults
     def __init__(self,
-                 # Define your operators params (with defaults) here
-                 # Example:
-                 # conn_id = your-connection-name
+                 redshift_conn_id="",
+                 sql_query="",
+                 table="",
+                 truncate="",
                  *args, **kwargs):
-
         super(LoadDimensionOperator, self).__init__(*args, **kwargs)
-        # Map params here
-        # Example:
-        # self.conn_id = conn_id
+        self.redshift_conn_id = redshift_conn_id
+        self.sql_query = sql_query
+        self.table = table
+        self.truncate = truncate
 
     def execute(self, context):
-        self.log.info('LoadDimensionOperator not implemented yet')
+        """
+          Insert data into dimensional tables from staging events and song data.
+          Using a truncate-insert method to empty target tables prior to load.
+        """
+        redshift = PostgresHook(postgres_conn_id=self.redshift_conn_id)
+        if self.truncate:
+            redshift.run(f"TRUNCATE TABLE {self.table}")
+        formatted_sql = self.sql_query.format(self.table)
+        redshift.run(formatted_sql)
+        self.log.info(f"Success: {self.task_id}")
