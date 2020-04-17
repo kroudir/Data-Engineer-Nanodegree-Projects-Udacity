5481d779d49339256584f6ef9af9d4c8e4e2afc5
Kroudir <a_kroudir@esi.dz>
2020-04-17 09:45:36 +0100

    feat:Implement DataQualityOperator

--- Project05-Data-pipeline/plugins/operators/data_quality.py
+++ Project05-Data-pipeline/plugins/operators/data_quality.py
@@ -2,21 +2,27 @@ from airflow.hooks.postgres_hook import PostgresHook
 from airflow.models import BaseOperator
 from airflow.utils.decorators import apply_defaults
 
+
 class DataQualityOperator(BaseOperator):
 
     ui_color = '#89DA59'
 
     @apply_defaults
     def __init__(self,
-                 # Define your operators params (with defaults) here
-                 # Example:
-                 # conn_id = your-connection-name
+                 aws_credentials_id="",
+                 redshift_conn_id="",
+                 tables=[],
                  *args, **kwargs):
-
         super(DataQualityOperator, self).__init__(*args, **kwargs)
-        # Map params here
-        # Example:
-        # self.conn_id = conn_id
+        self.aws_credentials_id = aws_credentials_id,
+        self.redshift_conn_id = redshift_conn_id,
+        self.tables = tables
 
     def execute(self, context):
-        self.log.info('DataQualityOperator not implemented yet')
+        redshift_hook = PostgresHook(self.redshift_conn_id)
+        for table in self.tables:
+            records = redshift_hook.get_records(f"SELECT COUNT(*) FROM {table}")
+            if len(records) < 1 or len(records[0]) < 1 or records[0][0] < 1:
+                self.log.error(f"Data quality check failed. {table} returned no results")
+                raise ValueError(f"Data quality check failed. {table} returned no results")
+            self.log.info(f"Data quality on table {table} check passed with {records[0][0]} records")
